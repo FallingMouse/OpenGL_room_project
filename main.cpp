69e5700ddc95bdff311a43fe9d479cedbd6ce67f
Kaewphet Nuruang <66639008+nobiofla@users.noreply.github.com>
2022-04-02 18:26:29 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -247,7 +247,7 @@ int main()
 
         //Clear window
         // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClearColor(186.0f/255.0f, 228.0f/255.0f, 228.0f/255.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //draw here
